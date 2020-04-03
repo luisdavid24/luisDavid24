58d2b118601fd797d4a0af0af087c0e21d39ce96
Luis david <david12.29@hotmail.com>
2020-04-02 20:47:22 -0500

    changed in the new.js

--- config/server.js
+++ config/server.js
@@ -7,3 +7,5 @@ app.set(app.set("port"),process.env.PORT||3000);
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"../app/views"));
 app.use(bodyParser.urlencoded({extended:false}));
+
+module.exports=app;
