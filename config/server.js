92b524fe281d10d332d7fb4ff1559b9fa192a69c
Luis david <david12.29@hotmail.com>
2020-04-02 20:43:43 -0500

    changedd

--- config/server.js
+++ config/server.js
@@ -4,3 +4,6 @@ const bodyParser =require("body-parser");
 const app=express();
 
 app.set(app.set("port"),process.env.PORT||3000);
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"../app/views"));
+app.use(bodyParser.urlencoded({extended:false}));
