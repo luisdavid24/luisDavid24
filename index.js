38103621301e28e1dc11b8930c20772d2ace65e0
Luis david <david12.29@hotmail.com>
2020-04-02 19:14:00 -0500

    changed 2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const server=require("server");
+const app=require("./config/server");
+
+require("./ap/routes/new")(app);
+//starting the server
+app.listen(app.get("port"),()=>{
+    console.log("server on port",app.get("port"));
+});
